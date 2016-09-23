f19bd7a1ad80851e12561441c9802fae3871b86d
ScorchRaserik <backupaccount10@gmail.com>
2016-09-23 16:29:33 -0700

    patch notes fix

--- scripts/patchnotes.js
+++ scripts/patchnotes.js
@@ -11,6 +11,7 @@
 //   hubot patch notes - Displays latest Warframe patch notes
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var jsdom = require("jsdom");
 
 module.exports = function(robot) {
 	robot.respond(/patch notes/i, function(msg) {
@@ -22,25 +23,35 @@ module.exports = function(robot) {
 		  {
 			var doc = x.responseText;
 			link = doc.split("<link>")[2].split("</link>")[0] + "\n";
+            msg.send(link);
+            
 			text = doc.split("<description>")[2].split("</description>")[0];
 			text = text.replace(/\n{2,}/g, "\n");
 			text = text.replace(/\<\!\[CDATA\[/g, "");
-			text = text.replace(/\]\]\>/g, "");
-			text = text.replace(/\<p\>/g, "");
-			text = text.replace(/\<\/p\>/g, "");
-			text = text.replace(/\<ul\>/g, "");
-			text = text.replace(/\<\/ul\>/g, "");
-			text = text.replace(/\<li\>/g, "");
-			text = text.replace(/\<\/li\>/g, "");
-			text = text.replace(/\<strong\>/g, "**");
-			text = text.replace(/\<\/strong\>/g, "**  ");
-			text = text.replace(/\&gt\;/g, ">");
-			text = text.replace(/\&lt\;/g, "<");
-			text = text.replace(/\&amp\;/g, "&");
-			text = text.replace(/\s{2,}/g, "\n * ");
-			text = text.replace(/\s\*\s\*\*/g, "\n**")
 			text = text.slice(0, -3);
-			msg.send(link + text);
+            jsdom.env(
+                text,
+                ["http://code.jquery.com/jquery.js"],
+                function (err, window) {
+                    var output = "";
+                    for(var i = 0; i < window.$("span").length; i++){
+                        var lineStart = "";
+                        var lineEnd = "";
+                        var style = window.$("span")[i].getAttribute("style");
+                        font = style.split("font-weight:")[1].split(";")[0];
+                        if(font == "700" || font == "blod"){
+                            lineStart = "\n**";
+                            lineEnd = "**\n";
+                        }
+                        else{
+                            lineStart = "* ";
+                            lineEnd = "\n";
+                        }
+                        output += lineStart + window.$("span")[i].innerHTML + lineEnd
+                    }
+                    msg.send(output);
+                }
+            );
 		  }
 		};
 		x.open("GET", "https://forums.warframe.com/forum/3-pc-update-build-notes.xml", true);
