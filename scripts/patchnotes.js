633b58e4a645bd94917d7a36f42c5c16e5cf497c
ScorchRaserik <backupaccount10@gmail.com>
2016-09-26 18:36:12 -0700

    patch notes fix

--- scripts/patchnotes.js
+++ scripts/patchnotes.js
@@ -29,29 +29,51 @@ module.exports = function(robot) {
 			text = text.replace(/\n{2,}/g, "\n");
 			text = text.replace(/\<\!\[CDATA\[/g, "");
 			text = text.slice(0, -3);
-            jsdom.env(
-                text,
-                ["http://code.jquery.com/jquery.js"],
-                function (err, window) {
-                    var output = "";
-                    for(var i = 0; i < window.$("span").length; i++){
-                        var lineStart = "";
-                        var lineEnd = "";
-                        var style = window.$("span")[i].getAttribute("style");
-                        font = style.split("font-weight:")[1].split(";")[0];
-                        if(font == "700" || font == "blod"){
-                            lineStart = "\n**";
-                            lineEnd = "**\n";
+            
+            //God Bless [DE]Megan's stylistic consistency
+            if(text.slice(1,4).startsWith("<p>") && text.slice(6,14).startsWith("<strong>")){
+                text = text.replace(/\]\]\>/g, "");
+                text = text.replace(/\<p\>/g, "");
+                text = text.replace(/\<\/p\>/g, "");
+                text = text.replace(/\<ul\>/g, "");
+                text = text.replace(/\<\/ul\>/g, "");
+                text = text.replace(/\<li\>/g, "");
+                text = text.replace(/\<\/li\>/g, "");
+                text = text.replace(/\<strong\>/g, "**");
+                text = text.replace(/\<\/strong\>/g, "**  ");
+                text = text.replace(/\&gt\;/g, ">");
+                text = text.replace(/\&lt\;/g, "<");
+                text = text.replace(/\&amp\;/g, "&");
+                text = text.replace(/\s{2,}/g, "\n * ");
+                text = text.replace(/\s\*\s\*\*/g, "\n**")
+                
+                msg.send(text);
+            }
+            else{
+                jsdom.env(
+                    text,
+                    ["http://code.jquery.com/jquery.js"],
+                    function (err, window) {
+                        var output = "";
+                        for(var i = 0; i < window.$("span").length; i++){
+                            var lineStart = "";
+                            var lineEnd = "";
+                            var style = window.$("span")[i].getAttribute("style");
+                            font = style.split("font-weight:")[1].split(";")[0];
+                            if(font == "700" || font == "blod"){
+                                lineStart = "\n**";
+                                lineEnd = "**\n";
+                            }
+                            else{
+                                lineStart = "* ";
+                                lineEnd = "\n";
+                            }
+                            output += lineStart + window.$("span")[i].innerHTML + lineEnd
                         }
-                        else{
-                            lineStart = "* ";
-                            lineEnd = "\n";
-                        }
-                        output += lineStart + window.$("span")[i].innerHTML + lineEnd
+                        msg.send(output);
                     }
-                    msg.send(output);
-                }
-            );
+                );
+            }
 		  }
 		};
 		x.open("GET", "https://forums.warframe.com/forum/3-pc-update-build-notes.xml", true);
